e4ee39b30743db7ebf6cc12df96a0f7f9a2f1746
Marvin Teichmann <marvin.teichmann@googlemail.com>
2016-10-21 18:55:15 +0100

    Final Version

--- encoder/vgg.py
+++ encoder/vgg.py
@@ -20,9 +20,8 @@ def inference(hypes, images, train=True):
     vgg_fcn = fcn8_vgg.FCN8VGG()
 
     num_classes = 2
-    with tf.name_scope("VGG"):
-        vgg_fcn.build(images, train=train, num_classes=num_classes,
-                      random_init_fc8=True)
+    vgg_fcn.build(images, train=train, num_classes=num_classes,
+                  random_init_fc8=True)
 
     vgg_dict = {'unpooled': vgg_fcn.conv5_3,
                 'deep_feat': vgg_fcn.pool5,
